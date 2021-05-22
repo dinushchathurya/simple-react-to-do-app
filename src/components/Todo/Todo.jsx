e74a4ea989d7da15ade772783a2ec6ccc7e72fd4
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-22 11:07:46 +0530

    feat: showing up single todo

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import { CircularProgress } from '@material-ui/core';
+import axios from 'axios';
 
 const Todo = () => {
+    const { id } = useParams();
+    const [todoDetails, setTodoDetails] = useState({});
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`).then((res) => {
+            const responseTodo = res.data;
+            setTodoDetails(responseTodo);
+        })
+    }, []);
+    
+    const { id: tododID, title, completed } = todoDetails;
     return (
-        <h2>This is Todo component</h2>
+        <div>
+            {todoDetails ?
+                (
+                    <div>
+                       <h2> {` ID: ${id} `} </h2>
+                       <h2> {`Title: ${title} `} </h2>
+                    </div>
+                ) : <CircularProgress />}
+        </div>
     );
 }
 
