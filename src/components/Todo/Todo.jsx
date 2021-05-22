d12acd07369cf25935cfcd0c520f9a510c7d9b8b
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-22 12:46:05 +0530

    bug: fix destructing error in Todo.jsx

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const Todo = () => {
     const { id } = useParams();
-    const [todoDetails, setTodoDetails] = useState({});
+    const [todoDetails, setTodoDetails] = useState();
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`).then((res) => {
             const responseTodo = res.data;
@@ -13,13 +13,13 @@ const Todo = () => {
         })
     }, []);
     
-    const { id: tododID, title, completed } = todoDetails;
+    const { id: todoID, title, completed } = todoDetails || {};
     return (
         <div>
             {todoDetails ?
                 (
                     <div>
-                       <h2> {` ID: ${id} `} </h2>
+                        <h2> {` ID: ${todoID} `} </h2>
                        <h2> {`Title: ${title} `} </h2>
                     </div>
                 ) : <CircularProgress />}
