c651e0e42419828e9215e3fe9e86943af5b5ebdc
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-22 11:07:04 +0530

    feat: peering TodoCard component

--- src/components/Todo/Todos.jsx
+++ src/components/Todo/Todos.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {CircularProgress} from '@material-ui/core';
+import TodoCard from './TodoCard';
 
 const Todos = () => {
+
+    const  [ todos, setTodos ] =useState();
+    useEffect(()=>{
+        axios.get(`https://jsonplaceholder.typicode.com/todos`).then((res)=>{
+            const responseData = res.data;
+            setTodos(responseData);
+        })
+    }, []);
     return (
-        <h2>This is Todos component</h2>
+        <React.Fragment>
+           { todos ? 
+           (
+                    <div style={{ display: "flex", flexWrap: "wrap" }} >
+                { todos.slice(0, 10).map(todo => <TodoCard key= {todo.id} todo={todo} /> )}
+              </div>
+           ): <CircularProgress /> }
+        </React.Fragment>
     );
 }
 
